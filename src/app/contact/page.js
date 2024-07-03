67d31a205f3c83915d74b2e91a56b3f2dc383a1e
Bima Mahendra <mahendrabima278@gmail.com>
2024-07-03 18:34:02 +0700

    add code license

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,19 @@
+/**
+ * This file is part of BizzCode Website Project.
+ *
+ * BizzCode Website Project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * BizzCode Website Project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BizzCode Website Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import HeaderPages from "@/components/HeaderPages";
