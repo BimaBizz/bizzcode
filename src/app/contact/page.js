ff48787cd856cc68b76e3077d0eaaa5fa70188ec
Bima Mahendra <mahendrabima278@gmail.com>
2024-07-05 19:56:17 +0700

    fix class component

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -41,7 +41,7 @@ export default function Contact() {
     <main className="relative overflow-hidden">
       <div className="background-radial-gradient absolute z-0 top-0 right-0 overflow-hidden h-full w-full hidden lg:block"></div>
       <Navbar />
-      <HeaderPages className={JostFont.className} title="Get in touch." subtitle="Contact me if you have any question about anything, I will be happy to help.">
+      <HeaderPages class={JostFont.className} title="Get in touch." subtitle="Contact me if you have any question about anything, I will be happy to help.">
         <p className="font-semibold mt-14" data-aos="fade-up" data-aos-duration="1500">Feel free to reach out to me 👇</p>
       </HeaderPages>
       <GetInTouch />
