51935c97e8f942c3f6345355afbfce4be768d21a
Bima Mahendra <mahendrabima278@gmail.com>
2024-07-07 20:17:01 +0700

    add jsonLd

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -63,15 +63,48 @@ async function getData({ slug }) {
   export default async function Page({ params }) {
     const data = await getData(params)
     const isi = data[0]
+
+    function convertTimestampToISO(timestamp) {
+      const date = new Date(timestamp * 1000);
+  
+      const year = date.getFullYear();
+      const month = ('0' + (date.getMonth() + 1)).slice(-2);
+      const day = ('0' + date.getDate()).slice(-2);
+      const hours = ('0' + date.getHours()).slice(-2);
+      const minutes = ('0' + date.getMinutes()).slice(-2);
+      const seconds = ('0' + date.getSeconds()).slice(-2);
+      const timezoneOffset = ('0' + (date.getTimezoneOffset() / -60)).slice(-2);
+  
+      const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+${timezoneOffset}:00`;
+  
+      return formattedDate;
+  }
+
+    const jsonLd = {
+      '@context': 'https://schema.org',
+      '@type': 'BlogPosting',
+      "headline": isi.titile,
+      "image": "https://admin.bizzcode.site/storage/uploads"+isi.image[0].path,
+      "datePublished": convertTimestampToISO(isi._created),
+      "author": [{
+          "@type": "Person",
+          "name": "Bima Mahendra",
+          "url": "https://bizzcode.site/about"
+        }]
+    }
    
     return (
         <main className="relative overflow-hidden">
-        <div className="background-radial-gradient absolute z-0 top-0 right-0 overflow-hidden h-full w-full hidden lg:block"></div>
-        <Navbar />
-        <SingleBlog data={isi} class={JostFont.className}/>
-        <div className="h-20"></div>
-        <Footer />
-        <div className="h-20"></div>
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+          />
+          <div className="background-radial-gradient absolute z-0 top-0 right-0 overflow-hidden h-full w-full hidden lg:block"></div>
+          <Navbar />
+          <SingleBlog data={isi} class={JostFont.className}/>
+          <div className="h-20"></div>
+          <Footer />
+          <div className="h-20"></div>
       </main>
     )
   }
